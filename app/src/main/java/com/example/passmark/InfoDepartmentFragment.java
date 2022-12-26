e023db7ffabf2e65411c801533c5a4fb3e98b07e
ZINE EDDINE <119061162+ZYNOCODES@users.noreply.github.com>
2022-12-26 05:19:32 +0100

    zynocodes commit

--- app/src/main/java/com/example/passmark/InfoDepartmentFragment.java
+++ app/src/main/java/com/example/passmark/InfoDepartmentFragment.java
@@ -1,40 +1,58 @@
 package com.example.passmark;
 
 import android.os.Bundle;
-
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.material.card.MaterialCardView;
 
 public class InfoDepartmentFragment extends Fragment {
     private View view;
-    private CardView L1mathinfo;
+    private MaterialCardView L1mathinfo,L2info,L3info;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_info_department, container, false);
-
-        L1mathinfo = view.findViewById(R.id.L1mathinfo);
+        InisializationOfFields();
         L1mathinfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction()
-                        .replace(R.id.navHostFragment, new FirstyearMathInfoFragment())
+                        .replace(R.id.fragment_container, new FirstyearMathInfoFragment())
+                        .addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+        L2info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, new SecondyearInfoFragment())
                         .addToBackStack(null);
                 fragmentTransaction.commit();
             }
         });
-
-
-
-
-
+        L3info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, new ThirdyearInfoFragment())
+                        .addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
         return view;
+    }
+    private void InisializationOfFields(){
+        L1mathinfo = view.findViewById(R.id.L1mathinfo);
+        L2info = view.findViewById(R.id.L2info);
+        L3info = view.findViewById(R.id.L3info);
     }
 }
--- app/src/main/java/com/example/passmark/MainActivity.java
+++ app/src/main/java/com/example/passmark/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.passmark;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -11,12 +14,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
     private ImageView menuicon;
     private NavController navController;
@@ -26,28 +30,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         drawerLayout = findViewById(R.id.drawerlayout);
-        menuicon = findViewById(R.id.menu);
-        menuicon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
-
-        navigationView = findViewById(R.id.navigationView);
-        navController = Navigation.findNavController(MainActivity.this,R.id.navHostFragment);
-        NavigationUI.setupWithNavController(navigationView,navController);
-        profile = findViewById(R.id.profile);
-        profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.navHostFragment,new ProfileFragment())
-                        .commit();
-
-            }
-        });
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new InfoDepartmentFragment()).commit();
+            navigationView.setCheckedItem(R.id.menuinformatique);
+        }
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuinformatique:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new InfoDepartmentFragment()).commit();
+                Toast.makeText(this, "Informatique Departement", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menumathematique:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MathDepartmentFragment()).commit();
+                Toast.makeText(this, "Coming Soon!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuelectronique:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ElectronicFragment()).commit();
+                Toast.makeText(this, "Coming Soon!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuchimie:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ChimieFragment()).commit();
+                Toast.makeText(this, "Coming Soon!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuphysique:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PhysiqueFragment()).commit();
+                Toast.makeText(this, "Coming Soon!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menumecanique:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MecaniqueFragment()).commit();
+                Toast.makeText(this, "Coming Soon!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
